0142bdec60620270e0f0f9e3c68876fdea1ab67e
Fox <jeffm@cwdlimted.com>
2011-03-24 15:42:55 -0400

    Migrate Fix

--- db/migrate/20110316165557_add_date.rb
+++ db/migrate/20110316165557_add_date.rb
@@ -1,7 +1,7 @@
 class AddDate < ActiveRecord::Migration
   def self.up
     change_table :posts do |t|
-      t.remove :date
+      
       t.date :date, :default => Time.now
     end
     Post.update_all ["date = ?", Time.now]
