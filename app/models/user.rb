5937175f3c0e8ed47671072ca1c473fbddd0dad3
Fox <jeffm@cwdlimted.com>
2011-03-30 12:10:54 -0400

    Password Encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
+require 'digest'
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
   
+  attr_accessor :password  #creates a virtal password attribute
+  
+  attr_accessible :name, :email, :password, :password_confirmation, :encrypted_password
+  
+  scope :marketing_user, where(:email => "marketing@cwdlimited.com")
+    
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name,  :presence => true,
@@ -10,5 +16,33 @@ class User < ActiveRecord::Base
                     :format   => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
                     
-  scope :marketing_user, where(:email => "marketing@cwdlimited.com")
+  # Automatically create the virtual attribute 'password_confirmation'.
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
+
+  before_save :encrypt_password
+  
+  def has_password?(submitted_password)
+    self.encrypted_password == encrypt(submitted_password)
+  end
+
+  private
+
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(self.password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{self.salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{self.password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
