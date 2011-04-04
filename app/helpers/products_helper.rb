a7d60bc9582205f29cb1215385b26a4ace99f72c
Nicholas Mulder <nicholas.mulder@gmail.com>
2011-04-03 22:02:40 -0400

    Refactor product show view; added new product_website_link helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,15 @@
 module ProductsHelper
+
+  def product_website_link(product, options = {})
+    options.reverse_merge!(:new_window => true)
+
+    link_options = {}
+    link_options[:target] = "_blank" if options[:new_window]
+
+    if product.url
+      link_to "View #{product.model}'s Website", product.url, link_options
+    else
+      link_to "View #{product.brand.name}'s Website", product.brand.url, link_options
+    end
+  end
 end
