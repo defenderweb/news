72efa3fa9f5a1765d3c4941fcd0c62df115f35f3
Fox <jeffm@cwdlimted.com>
2011-04-04 12:54:15 -0400

    Added login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,32 @@
 class SessionsController < ApplicationController
+  
+  before_filter :set_page_title
+  
   def new
+  
   end
+  
+  def create
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combination."
+      render 'new'
+    else
+      sign_in user
+      redirect_to user
+    end
+  end
+  
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
+  
+  private
 
+    def set_page_title
+      page_title << "Sign In"
+    end
+  
 end
