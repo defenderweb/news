e12d5273346e639c64a1660024403a2246fba07a
Nicholas Mulder <nicholas.mulder@gmail.com>
2011-04-03 20:47:17 -0400

    Only use auth in production mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-   
+
   private
- 
+
   def authenticate
     authenticate_or_request_with_http_basic do |user_name, password|
       (user_name == 'admin' && password == 'foobar') || (user_name == 'jeffm@cwdlimited.com' && password == 'foobar')
-    end
+    end if Rails.env.production?
   end
 end
